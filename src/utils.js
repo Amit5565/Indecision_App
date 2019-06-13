5ace1cd601f0ba78a84909400ab621c08023e31e
Amit Kumar Singh <amit.ks30999@gmail.com>
2019-06-14 01:25:13 +0530

    webpack import and export

---
+++ src/utils.js
@@ -0,0 +1,16 @@
+console.log("utils.js is running");
+
+export const square=(x)=>{
+    return x*x
+}
+
+
+export const add=(a,b)=>{
+    return a+b;
+}
+
+const subtract=(a,b) => a-b;
+export default subtract
+//exports- default export & named export
+
+//-> Named exports export{ square, add };
